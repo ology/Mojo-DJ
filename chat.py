8099c36263ef66de8bc1dda817c21cd6729fd672
Gene Boggs <gene@ology.net>
2025-06-14 22:52:50 -0700

    WIP

--- chat.py
+++ chat.py
@@ -3,12 +3,12 @@ from google.genai import types
 import os
 import sys
 
-def get_response(prompt):
+def get_response(instruction, prompt):
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         config=types.GenerateContentConfig(
-            system_instruction="Detail the history of the given song.",
+            system_instruction=instruction,
             max_output_tokens=1000,
             temperature=0
         ),
@@ -19,10 +19,11 @@ def get_response(prompt):
 
 def main():
     response = ""
-    if sys.argv[1]:
-        prompt = sys.argv[1]
+    if sys.argv[1] and sys.argv[2]:
+        instruction = sys.argv[1]
+        prompt = sys.argv[2]
         if prompt:
-            response = get_response(prompt)
+            response = get_response(instruction, prompt)
     print(response)
 
 if __name__ == "__main__":
